5defbab0993d9ad1b79848faa6a6b25f5fa28b9b
czarjulius <juliusczar.jc@gmail.com>
2020-06-25 14:35:03 +0200

    fixed insecuredirect object reference

--- app/controllers/benefit_forms_controller.rb
+++ app/controllers/benefit_forms_controller.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 class BenefitFormsController < ApplicationController
-
   def index
     @benefits = Benefits.new
   end
 
   def download
-   begin
-     path = params[:name]
-     file = params[:type].constantize.new(path)
-     send_file file, disposition: "attachment"
-   rescue
-     redirect_to user_benefit_forms_path(user_id: current_user.id)
-   end
+    begin
+      path = params[:name]
+      if path == "public/docs/Dental_n_Stuff.pdf" || path == "public/docs/Health_n_Stuff.pdf"
+        file = params[:type].constantize.new(path)
+        send_file file, disposition: "attachment"
+      else
+        render json: {},status: :bad_request
+      end
+    rescue
+      redirect_to user_benefit_forms_path(user_id: current_user.id)
+    end
   end
 
   def upload
@@ -25,5 +28,4 @@ class BenefitFormsController < ApplicationController
     end
     redirect_to user_benefit_forms_path(user_id: current_user.id)
   end
-
 end
--- app/controllers/work_info_controller.rb
+++ app/controllers/work_info_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 class WorkInfoController < ApplicationController
-  def index
-    @user = current_user
-    if !(@user) || @user.admin
-      flash[:error] = "Apologies, looks like something went wrong"
-      redirect_to home_dashboard_index_path
-    end
+def index
+  @user = current_user
+  if !(@user) || @user.admin
+    flash[:error] = "Apologies, looks like something went wrong"
+    redirect_to home_dashboard_index_path
   end
+end
 
 end
